001981a885251588bd6ef8efe5ec1bfcd2fb7518
Umid <32@.ru>
2023-09-24 21:58:09 +0500

    dz3

---
+++ app.js
@@ -0,0 +1,27 @@
+// Первое задание 
+//let num = +prompt('Введите число')
+//let deg = +prompt('Введите степень')
+//let result = 1;
+
+    //result = num * result
+//for(let i = 0; i < deg; i++){
+  //  result = num * result
+//}
+
+//alert(result)
+
+// Второе задание
+
+//let amount = +prompt('Введите количество ступенек')
+//let symvol = prompt('Введите символ')
+//let last = prompt('Введите конечный символ')
+//let res = ''
+//for(let i = 0; i < amount; i++){
+   
+  // if(i == 0){
+   //     res = last
+   // } else {
+    //   res = symvol + res
+    //}
+    //console.log(res);
+//}
